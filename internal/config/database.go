2e0187562e2c269df20cd29c19163e455affe042
TCO46 <patohru2006@gmail.com>
2025-08-31 12:06:18 +0700

    fix: Rename database config

--- internal/config/database.go
+++ internal/config/database.go
@@ -3,11 +3,11 @@ package config
 import "fmt"
 
 type DatabaseConfig struct {
-	Database string `env:"DB_DATABASE"`
-	Username string `env:"DB_USERNAME"`
-	Password string `env:"DB_PASSWORD"`
+	Database string `env:"DATABASE_DB"`
+	Username string `env:"DATABASE_USER"`
+	Password string `env:"DATABASE_PASS"`
 	Port     int    `env:"DB_PORT" envDefault:"5432"`
-	Host     string `env:"DB_HOST" envDefault:"localhost"`
+	Host     string `env:"DATABASE_HOST" envDefault:"localhost"`
 }
 
 func (cfg *DatabaseConfig) DatabaseURL() string {
